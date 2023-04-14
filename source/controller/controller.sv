ceef60a4927ca3dce7987a9a66ce6ac70188eed2
Timothy Hein <heint@purdue.edu>
2023-04-14 17:35:58 -0400

    controller heirarchy

--- source/controller/controller.sv
+++ source/controller/controller.sv
@@ -2,10 +2,22 @@ module controller(
 
 );
 
-// forwarding
+/** forwarding
+ *  detection: if command in execution stage depends on a value that is being written
+ *    regwrite & (write_addr == addr_a | write_addr == addr_b)
+ *  handling:  stall execution until write data has wrapped around
+**/
 
-// jump
+/** jump
+ *  detection: if branching
+ *    branch
+ *  handling: if conditional branching wait for flags, wait for pc before continuing also
+**/
 
-// memeory hazard
+/** memeory hazard
+ *  detection: if reading from memory that has recently been written to
+ *    memtoreg & writereg
+ *  handling: wait until the write has been completed
+**/
 
 endmodule
